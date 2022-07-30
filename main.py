70c7ab37ae216b9cdde40b1ed0f27e5abdb7988f
ivelinamisova <ivelina_misova@abv.bg>
2022-07-30 12:41:02 +0300

    init

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 name = input('Enter your name: ')
-adjective = input('Enter your adjective: ')
-superlative = input('Enter your superlative: ')
-noun = input('Enter your noun: ')
 
-print( )
+adjactive = input('enter adjective : ')
+color = input('enter a color name : ')
+thing = input('enter a thing name :')
+place = input('enter a place name : ')
+person = input('enter a person name : ')
+adjactive1 = input('enter a adjactive1 : ')
+insect = input('enter a insect name : ')
+food = input('enter a food name : ')
+verb = input('enter a verb name : ')
+print(
+    'Last night I dreamed I was a '
+    + adjactive + ' butterfly with '
+    + color + ' splocthes that looked like '
+    + thing + ' .I flew to '
+    + place + ' with my bestfriend and '
+    + person + ' who was a '
+    + adjactive1 + ' '
+    + insect + ' .We ate some '
+    + food + ' when we got there and then decided to '
+    + verb + ' and the dream ended when I said-- lets '
+    + verb + '.')
